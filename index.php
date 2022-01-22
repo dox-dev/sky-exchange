83791702917f29ca7a6894b2ea8118fe84151c53
Dox <doxofficial1235@gmail.com>
2022-01-22 17:04:42 +0100

    Update

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <link rel="stylesheet" href="/main.css">
+    <link rel="shortcut icon" href="/images/favicon.ico">
+    <title>SkyExchange | Decentralized Duino Coin Exchange</title>
+</head>
+<body>
+    <div class="loader-wrapper">
+        <div class="loader"><div></div><div></div></div>
+    </div>
+
+    <script>
+        $(window).on("load", function() {
+            $(".loader-wrapper").delay(500).fadeOut("slow");
+        });
+    </script>
+</body>
+</html>
